beaca2188f1ed7f4756b883e1279b786785c920e
Michal Novotny <minovotn@redhat.com>
2011-07-22 15:23:27 +0200

    Implement version check to the init.php script to check for correct version of libvirt-php
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- init.php
+++ init.php
@@ -1,4 +1,10 @@
 <?php
+	define('LOGDIR', getcwd().'/logs');
+	define('LIBVIRT_PHP_REQ_VERSION', '0.4.3');
+	define('PHPVIRTCONTROL_VERSION', '0.0.2');
+	define('PHPVIRTCONTROL_WEBSITE', 'http://www.php-virt-control.org');
+	define('ALLOW_EXPERIMENTAL_VNC', false);
+
 	session_start();
 
 	if (array_key_exists('lang-override', $_GET)) {
@@ -19,11 +25,6 @@
 	else
 		$lang_str = $_SESSION['language'];
 
-	define('LOGDIR', getcwd().'/logs');
-	define('PHPVIRTCONTROL_VERSION', '0.0.1');
-	define('PHPVIRTCONTROL_WEBSITE', 'http://www.php-virt-control.org');
-	define('ALLOW_EXPERIMENTAL_VNC', false);
-
 	if (!File_Exists(LOGDIR)) {
 		if (!mkdir(LOGDIR, 0777))
 			define(LOGDIR, false);
@@ -36,5 +37,19 @@
 	require('classes/database-file.php');
 
 	$lang = new Language($lang_str);
+
+	/* Check for libvirt-php */
+	if (!function_exists('libvirt_check_version')) {
+		include('error-missing.php');
+		exit;
+	}
+
+	/* Now check for correct version of libvirt-php */
+	$tmp = explode('.', LIBVIRT_PHP_REQ_VERSION);
+	if (!libvirt_check_version($tmp[0], $tmp[1], $tmp[2])) {
+		include('error-need-update.php');
+		exit;
+	}
+
 	$db = getDBObject('file:data/test.dat');
 ?>
